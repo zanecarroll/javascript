3f0a1843e2be964e1432f995342ed20efb476f32
zanecarroll <64847449+zanecarroll@users.noreply.github.com>
2020-05-05 10:37:19 -0400

    Add files via upload

---
+++ Javascript/assets/javascript/mycoffeescript.js
@@ -0,0 +1,88 @@
+
+
+
+
+var cups = 2;
+
+if (cups < 3) {
+document.getElementById("viewcounter").innerHTML = "Yes Ill have another cup of coffee";
+
+}else{
+
+document.getElementById("viewcounter").innerHTML = "I think I'm okay for now";
+
+console.log("this appears");
+}
+
+
+
+
+
+
+
+
+
+
+
+var x = 1;//keeps track of how many people have viewed my website
+
+		var y = "2";
+		var z = true;
+
+
+		var show = 0;
+
+		if (show) {
+		
+	document.getElementById("viewcounter").innerHTML = x;
+}else if (show == 5){
+
+document.getElementById("viewcounter").innerHTML = "";
+
+}else{
+
+document.getElementById("viewcounter").innerHTML = "IDK what to do";
+
+console.log("this appears 111")
+
+}
+
+
+
+
+
+
+
+
+var temp = 81;
+var prec = false;
+var indoors = false;
+
+console.log("The temperature is " + temp + 'degrees');
+
+if (temp >80 && !prec || indoors == true){
+	document.getElementById("viewcounter").innerHTML = "time to swim";
+
+
+
+}else{
+
+document.getElementById("viewcounter").innerHTML = "stay inside";
+
+console.log("this appears 111");
+
+}
+for (var bottles = 99; bottles > 0; bottles--) {
+	console.log( "we have" + bottles + "bottles of beer on the wall");
+}
+
+while(x < 10){
+	console.log("x is still less than 10!");
+	x++;
+}
+
+
+
+
+
+
